bb5b569ec4b25e1f82e4e50be84e69e17c235928
danwycliff <danwycliff6769@gmail.com>
2023-10-19 16:58:51 +0300

    codes

--- server/app.py
+++ server/app.py
@@ -19,5 +19,18 @@ db.init_app(app)
 def index():
     return "Index for Game/Review/User API"
 
+@app.route('/games/<int:id>')
+def game_by_id(id):
+    game = Game.query.filter(Game.id ==id).first()
+    
+    game_dict = game.to_dict()
+
+    response = make_response(
+        jsonify(game_dict),
+        200
+    )
+    response.headers["Content-Type"] = "application/json"
+    return response
+
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(port=5555)
